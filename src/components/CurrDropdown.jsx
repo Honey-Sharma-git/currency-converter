7b0e8b189ce1cde4a9cb665d05619f1da52fccaf
Honey-sharma-git <honeysharmaemail@gmail.com>
2025-04-01 18:35:26 +0530

    Set basic UI

--- src/components/CurrDropdown.jsx
+++ src/components/CurrDropdown.jsx
@@ -1,50 +1,64 @@
 import { currSymbolData } from "../utils/currencySymbolData";
 export const CurrDropdown = ({ handleChange, userInput }) => {
   return (
-    <>
-      <label htmlFor="fromCurr">From currency:</label>
-      <br />
-      {userInput.fromCurr && (
-        <img src={`https://flagsapi.com/${userInput.fromCurr}/flat/64.png`} />
-      )}
-      <select
-        onChange={(e) => {
-          handleChange(e);
-        }}
-        name="fromCurr"
-        id="fromCurr"
-      >
-        <option value="From Currency">From Currency</option>
-        {currSymbolData.map((symbol, index) => {
-          return (
-            <option key={index} value={symbol.countryCode}>
-              {symbol.name} {symbol.countryCode}
-            </option>
-          );
-        })}
-      </select>
-      <br />
-      <label htmlFor="toCurr">From currency:</label>
-      <br />
-      {userInput.toCurr && (
-        <img src={`https://flagsapi.com/${userInput.toCurr}/flat/64.png`} />
-      )}
-      <select
-        onChange={(e) => {
-          handleChange(e);
-        }}
-        name="toCurr"
-        id="toCurr"
-      >
-        <option value="To currency">To Currency</option>
-        {currSymbolData.map((symbol, index) => {
-          return (
-            <option key={index} value={symbol.countryCode}>
-              {symbol.name} {symbol.countryCode}
-            </option>
-          );
-        })}
-      </select>
-    </>
+    <div className="flex flex-row gap-3">
+      <div className="flex flex-col">
+        <label htmlFor="fromCurr">From currency:</label>
+        <div className="flex flex-row gap-3 border">
+          <figure className="w-10 aspect-square  ">
+            {userInput.fromCurr && (
+              <img
+                src={`https://flagsapi.com/${userInput.fromCurr}/flat/64.png`}
+              />
+            )}
+          </figure>
+
+          <select
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            name="fromCurr"
+            id="fromCurr"
+          >
+            <option value="From Currency">From Currency</option>
+            {currSymbolData.map((symbol, index) => {
+              return (
+                <option key={index} value={symbol.countryCode}>
+                  {symbol.name} {symbol.countryCode}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="toCurr">From currency:</label>
+        <div className="flex flex-row gap-3 border">
+          <figure className="w-10 aspect-square">
+            {userInput.toCurr && (
+              <img
+                src={`https://flagsapi.com/${userInput.toCurr}/flat/64.png`}
+              />
+            )}
+          </figure>
+          <select
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            name="toCurr"
+            id="toCurr"
+          >
+            <option value="To currency">To Currency</option>
+            {currSymbolData.map((symbol, index) => {
+              return (
+                <option key={index} value={symbol.countryCode}>
+                  {symbol.name} {symbol.countryCode}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
+    </div>
   );
 };
--- src/pages/Converter.jsx
+++ src/pages/Converter.jsx
@@ -1,10 +1,9 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 
 import { CurrDropdown } from "../components/CurrDropdown";
 export const Converter = () => {
   const [userInput, setUserInput] = useState({
-    amount: "",
+    amount: 100,
     fromCurr: "",
     toCurr: "",
   });
@@ -13,14 +12,14 @@ export const Converter = () => {
       return { ...prev, [e.target.name]: e.target.value };
     });
   }
-
+  console.log(userInput);
   return (
-    <main>
-      <form>
-        <legend>Currency converter</legend>
-        <br />
+    <main className="flex flex-row justify-center  items-center min-h-screen">
+      <form className="flex flex-col gap-3">
+        <legend className="text-center font-bold text-2xl">
+          Currency converter
+        </legend>
         <label htmlFor="amount">Enter amount:</label>
-        <br />
         <input
           onChange={(e) => {
             handleChange(e);
@@ -28,14 +27,15 @@ export const Converter = () => {
           type="text"
           name="amount"
           id="amount"
+          className="border"
+          value={userInput.amount}
         />
-        <br />
         <CurrDropdown userInput={userInput} handleChange={handleChange} />
-        <br />
-        <button>Get exchange rate</button>
+        <button className="border cursor-pointer">Get exchange rate</button>
+        <p className="text-center border-dashed border p-2">
+          Exchange rates will appear here
+        </p>
       </form>
-      <br />
-      <p></p>
     </main>
   );
 };
