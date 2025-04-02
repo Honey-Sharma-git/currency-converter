3c03d2a07bdab25840fbc50fce88c20c8a71d02d
Honey-sharma-git <honeysharmaemail@gmail.com>
2025-04-02 15:05:30 +0530

    Refactor: Renamed variables and adjusted order of code

--- src/pages/Converter.jsx
+++ src/pages/Converter.jsx
@@ -2,28 +2,26 @@ import { useEffect, useState } from "react";
 import { CurrDropdown } from "../components/CurrDropdown";
 import axios from "axios";
 export const Converter = () => {
-  const baseURL =
-    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1";
+  const baseURL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@";
+  const [conversionFactor, setConversionFactor] = useState("");
+  const [exchangeRates, setExchangeRates] = useState("");
   const [userInput, setUserInput] = useState({
     amount: 100,
     fromCurr: "",
-    fromCountryCode: "",
     toCurr: "",
-    toCountryCode: "",
   });
-  console.log(userInput);
-  const [convertedAmt, setConvertedAmt] = useState("");
-  const [exchangeRates, setExchangeRates] = useState("");
+
   function handleChange(e) {
     setUserInput((prev) => {
       return { ...prev, [e.target.name]: e.target.value };
     });
   }
+
   async function getData() {
     try {
       if (userInput.amount && userInput.fromCurr && userInput.toCurr) {
         const response = await axios.get(
-          `${baseURL}/currencies/${userInput.fromCurr}.json`
+          `${baseURL}latest/v1/currencies/${userInput.fromCurr}.json`
         );
         if (response.status === 200) {
           setExchangeRates(response.data);
@@ -33,19 +31,22 @@ export const Converter = () => {
       console.log("Error calling Currency exchange API:", error);
     }
   }
+
   useEffect(() => {
     if (userInput.fromCurr && userInput.toCurr && exchangeRates) {
-      setConvertedAmt(
+      setConversionFactor(
         exchangeRates[userInput.fromCurr]?.[userInput.toCurr] || "N/A"
       );
     }
   }, [exchangeRates]);
+
   return (
     <main className="flex flex-row justify-center  items-center min-h-screen">
       <form className="flex flex-col gap-3">
         <legend className="text-center font-bold text-2xl">
           Currency converter
         </legend>
+        
         <label htmlFor="amount">Enter amount:</label>
         <input
           onChange={(e) => {
@@ -57,7 +58,9 @@ export const Converter = () => {
           className="border"
           value={userInput.amount}
         />
+
         <CurrDropdown userInput={userInput} handleChange={handleChange} />
+
         <button
           onClick={(e) => {
             e.preventDefault();
@@ -67,9 +70,10 @@ export const Converter = () => {
         >
           Get exchange rate
         </button>
+
         <p className="text-center border-dashed border p-2">
-          {convertedAmt
-            ? (convertedAmt * userInput.amount).toFixed(2)
+          {conversionFactor
+            ? (conversionFactor * userInput.amount).toFixed(2)
             : "Result will appear here"}
         </p>
       </form>
